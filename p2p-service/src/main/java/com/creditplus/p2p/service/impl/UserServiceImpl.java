bc2194b3e7a2272e3a47dd53fd6ad5c790a555b9
yishell <yishell.com@gmail.com>
2015-10-04 17:54:32 +0800

    restore

---
+++ p2p-service/src/main/java/com/creditplus/p2p/service/impl/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package com.creditplus.p2p.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.creditplus.p2p.dao.UserDao;
+import com.creditplus.p2p.model.UserVO;
+import com.creditplus.p2p.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	@Autowired 
+	private UserDao userDao;
+		
+	public void addUser(UserVO userVO) {
+		userDao.insertUser(userVO);
+	}
+	
+	public void deleteUserById(int userId) {
+		userDao.deleteUserById(userId);
+	}
+
+	public void updateUser(UserVO userVO) {
+		userDao.updateUser(userVO);
+	}
+	
+	public UserVO getUserById(int userId) {
+		return userDao.getUserById(userId);
+	}
+	
+	public List<UserVO> getUsersWithPage(){
+		return null;
+	}
+}
