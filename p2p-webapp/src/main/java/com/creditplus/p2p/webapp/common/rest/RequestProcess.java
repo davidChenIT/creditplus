c05a921cfc54fce373dcc19a158c5f813dcc2444
Administrator <Administrator@PC201306252132>
2015-10-04 15:51:14 +0800

    sdfsd

--- p2p-webapp/src/main/java/com/creditplus/p2p/webapp/common/rest/RequestProcess.java
+++ p2p-webapp/src/main/java/com/creditplus/p2p/webapp/common/rest/RequestProcess.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Method;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -20,7 +21,7 @@ import com.creditplus.p2p.util.JsonUtils;
 @Produces(MediaType.APPLICATION_JSON)
 public class RequestProcess{
 
-	@POST
+	@GET
 	@Path("/{module}/{method}/{request_data}")
 	@Produces(MediaType.APPLICATION_JSON)	
 	@Consumes(MediaType.APPLICATION_XML)
