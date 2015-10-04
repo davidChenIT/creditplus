20ed62e9ce86623a273852238f10348652ef8463
yishell <yishell.com@gmail.com>
2015-10-04 17:58:33 +0800

    add

--- p2p-webapp/src/main/java/com/creditplus/p2p/webapp/common/rest/RequestProcess.java
+++ p2p-webapp/src/main/java/com/creditplus/p2p/webapp/common/rest/RequestProcess.java
@@ -1,104 +1,98 @@
 package com.creditplus.p2p.webapp.common.rest;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
 import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
-import com.creditplus.p2p.annotation.MethodParamType;
-import com.creditplus.p2p.util.JsonUtils;
+import com.creditplus.p2p.common.annotation.MethodDesc;
+import com.creditplus.p2p.common.annotation.ParamDesc;
+import com.creditplus.p2p.common.json.JSONTools;
 
 @Path(value="/")
 @Produces(MediaType.APPLICATION_JSON)
 public class RequestProcess{
 
-	@GET
-	@Path("/{module}/{method}/{request_data}") 
+	@POST
+	@Path("/{module}/{method}/{request_data}")
 	@Produces(MediaType.APPLICATION_JSON)	
 	@Consumes(MediaType.APPLICATION_XML)
-	public Object execute(@PathParam("module")String module, @PathParam("method")String method, @PathParam("request_data")String request_data,@Context HttpServletRequest servletRequest) throws Exception  {
-		Object result=null;
-		Map requestMap=JsonUtils.json2Map(request_data);
-		System.out.println("======servletContext"+servletRequest.getServletContext());
-		ApplicationContext context=WebApplicationContextUtils.getRequiredWebApplicationContext(servletRequest.getServletContext());
-		System.out.println("======contJson"+JsonUtils.object2Json(context));
-		Object targetObj=context.getBean(module);
-		
-		if(targetObj==null)
-			throw new Exception(new StringBuilder("request module='").append(module).append("' is not exist!").toString());
-		//找到带有MethodParamType注解的方法
-		Method methods[]=targetObj.getClass().getDeclaredMethods();
-		Method m=null;
-		for(Method methodObj:methods){
-			if(methodObj.getName().equals(method) && methodObj.getAnnotation(MethodParamType.class)!=null){
-				m=methodObj;
-				break;
-			}
-			
+	public Object execute(@PathParam("module")String module,
+			@PathParam("method")String method, 
+			@PathParam("request_data")String request_data,@Context HttpServletRequest request) throws Exception {
+		if(StringUtils.isBlank(module)){
+			throw new Exception("module is error,please check it exist!");
 		}
 		
-		if(m==null)
-			throw new Exception(new StringBuilder("request method='").append(method).append("' is not exist!").toString());
-		MethodParamType methodParamType=m.getAnnotation(MethodParamType.class);
-		String []paramKey=methodParamType.paramKey();
-		Class  []paramType=methodParamType.paramType();
-		Object []objValue=new Object[paramKey.length];
-		for(int z=0;z<paramKey.length;z++){
-			Class clz=Object.class;
-			if(z<paramType.length){
-				clz=paramType[z];
-			}
-			objValue[z]=JsonUtils.json2Object(JsonUtils.object2Json(requestMap.get(paramKey[z])), clz);
+		if(StringUtils.isBlank(method)){
+			throw new Exception("method is error,please check it exist!");
 		}
-		result=m.invoke(targetObj, objValue);
-		return result;
+		
+		ApplicationContext appContext = WebApplicationContextUtils.getRequiredWebApplicationContext(request.getServletContext());
+		Object service = appContext.getBean(module);
+		if(null == service){
+			throw new Exception("module is error,please check it exist!");
+		}
+		
+        Method classMethod = this.getMethod(service, method);
+        if(null == classMethod){
+			throw new Exception("method is error,please check it exist!");        	
+        }
+		
+        Object[] args = getArgs(classMethod,request_data);        
+        Object object = classMethod.invoke(service, args);
+        if((classMethod.getReturnType()) == void.class) {
+        	Map<String, Boolean> rsMap = new HashMap<String, Boolean>();
+        	rsMap.put("result", true);
+        	return rsMap;
+        }
+               
+		return object;
 	}
 	
-	/*@SuppressWarnings("rawtypes")
-	public static void main(String[] args) throws Exception {
-		String module="",method = "updateUser";
-		String userJson="{\"userVo\":{\"username\":\"23242\"}}";
-		Map requestMap=JsonUtils.json2Map(userJson);
-		Object targetObj=new UserServiceImpl();
+	private Method getMethod(Object object,String methodName){
+		Method[] classMethods = object.getClass().getMethods();
+		for(Method classMethod : classMethods){
+			if(methodName.equals(classMethod.getName()) && Modifier.isPublic(classMethod.getModifiers())){
+				return classMethod;
+			}
+		}
 		
-		if(targetObj==null)
-			throw new Exception(new StringBuilder("request module='").append(module).append("' is not exist!").toString());
-		Method methods[]=targetObj.getClass().getDeclaredMethods();
-		Method m=null;
-		for(Method methodObj:methods){
-			System.out.println("==="+methodObj.getName());
-			if(methodObj.getName().equals(method) && methodObj.getAnnotation(MethodParamType.class)!=null){
-				m=methodObj;
-				break;
-			}
-			
-		}
-		if(m==null)
-			throw new Exception(new StringBuilder("request method='").append(method).append("' is not exist!").toString());
-		MethodParamType methodParamType=m.getAnnotation(MethodParamType.class);
-		String []paramKey=methodParamType.paramKey();
-		Class  []paramType=methodParamType.paramType();
-		Object []objValue=new Object[paramKey.length];
-		for(int z=0;z<paramKey.length;z++){
-			Class clz=Object.class;
-			if(z<paramType.length){
-				clz=paramType[z];
-			}
-			objValue[z]=JsonUtils.json2Object(JsonUtils.object2Json(requestMap.get(paramKey[z])), clz);
-		}
-		Object result = m.invoke(targetObj, objValue);
-	}*/
+		return null;
+	}
 	
-	
-
-	
+	private Object[] getArgs(Method classMethod,String requestData)throws Exception{
+        MethodDesc methodDesc = classMethod.getAnnotation(MethodDesc.class);
+        if(null == methodDesc){
+        	return null;
+        }
+        
+        ParamDesc[] paramDescs = methodDesc.Desc();
+        if(null == paramDescs || paramDescs.length <=  0){
+        	return null;
+        }
+        
+        Object[] args = new Object[paramDescs.length];
+        for(int i = 0;i < paramDescs.length;i++){
+        	ParamDesc paramDesc = paramDescs[i];
+        	String paramName = paramDesc.paramName();
+        	Class<?> paramClass = paramDesc.paramType();
+        	args[i] = JSONTools.JSON2Object(requestData, paramName, paramClass);
+        }
+        
+        return args;
+	}	
 }
