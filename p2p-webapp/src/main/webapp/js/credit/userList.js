9d6a792297131a9c61e7f1bd5d102808a8b23800
wumingtuo <4591779252@qq.com>
2015-10-05 16:10:43 +0800

    绣改userList

--- p2p-webapp/src/main/webapp/js/credit/userList.js
+++ p2p-webapp/src/main/webapp/js/credit/userList.js
@@ -1,22 +1,22 @@
 //页面初始化加载函数
 $(function(){
 	debugger;
+	var serviceAddress="http://"+window.location.host+"/p2p-webapp/services/process";
 	//构造grid
     $("#userListGrid").jqGrid({
-			url:'services/process',
+			url:serviceAddress,
 			datatype: 'json',
-			data:{"module":"userService","method":"getUserListWithPage","request_data":{}},
+			postData:{"module":"userService","method":"getUserListWithPage","request_data":{}},
 			mtype: 'psot',
 			autowidth:true,
 			colNames:['操作','用户名称'],
 			colModel :[
-				{name:'operate_col', index:'operate_col',align:'center',width:"20%"},
-				{name:'username', index:'username',align:'center',width:"80%"}
+				{name:'operate_col', index:'operate_col',align:'center',width:"10%","sortable":false},
+				{name:'username', index:'username',align:'center',width:"90%","sortable":false}
 			],
 			pager: '#userListPager',
 			rowNum:10,
 			rowList:[10,20,30],
 			viewrecords: true
-			//caption: 'My first grid'
 	});
 })
