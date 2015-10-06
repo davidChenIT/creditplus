37d949f8d20c0e342be034e2b0aa620dd84b615a
wumingtuo <4591779252@qq.com>
2015-10-06 21:05:51 +0800

    修改信用初审列表

--- p2p-webapp/src/main/webapp/js/credit/firstTrialList.js
+++ p2p-webapp/src/main/webapp/js/credit/firstTrialList.js
@@ -1,31 +1,43 @@
 //页面初始化加载函数
 $(function(){
 	debugger;
+	var serviceAddress="http://"+window.location.host+"/p2p-webapp/services/process";
 	//构造grid
- $("#firstTrialListGrid").jqGrid({
-	/**url:'example.php',
-	datatype: 'xml',
-	mtype: 'GET',*/
-	autowidth:true,
-	colNames:['申请单编号','申请人姓名','申请人身份证号','金额','期次','时长','申请时间','申请单状态','初审人','复审人'],
-	colModel :[
-		{name:'applay_code', index:'applay_code',align:'center'},
-		{name:'applay_user', index:'applay_user',align:'center'},
-		{name:'applay_user_card', index:'applay_user_card',align:'center'},
-		{name:'money', index:'money',align:'center'},
-		{name:'qici', index:'qici',align:'center'},
-		{name:'time', index:'time',align:'center'},
-		{name:'applay_date', index:'applay_date',align:'center'},
-		{name:'applay_status', index:'applay_status',align:'center'},
-		{name:'approve_first_user', index:'approve_first_user',align:'center'},
-		{name:'approve_last_user', index:'approve_last_user',align:'center'}
-	],
-	pager: '#firstTrialListPager',
-	rowNum:10,
-	rowList:[10,20,30],
-	sortname: 'invid',
-	sortorder: 'desc',
-	viewrecords: true
-	//caption: 'My first grid'
+	$("#firstTrialListGrid").jqGrid({
+			url:serviceAddress,
+			datatype: 'json',
+			postData:{"module":"loanOrderService","method":"getLoanOrderList","request_data":{}},
+			mtype: 'POST',
+			autowidth:true,
+			colNames:["申请编号","申请人姓名","申请人身份证号","金额","期次","时长","申请时间","申请单状态","初审人","复审人"],
+			colModel :[
+				{name:'loan_id', index:'loan_id',align:'center',"sortable":false},
+				{name:'name', index:'name',align:'center',"sortable":false},
+				{name:'id_num', index:'id_num',align:'center',"sortable":false},
+				{name:'loan_money', index:'loan_money',align:'center',"sortable":false},
+				{name:'dateCount', index:'dateCount',align:'center',"sortable":false},
+				{name:'loan_day', index:'loan_day',align:'center',"sortable":false},
+				{name:'start_day', index:'start_day',align:'center',"sortable":false},
+				{name:'state', index:'state',align:'center',"sortable":false},
+				{name:'first_approver', index:'first_approver',align:'center',"sortable":false},
+				{name:'review_approver', index:'review_approver',align:'center',"sortable":false}
+			],
+			pager: '#firstTrialListPager',
+			rowNum:10,
+			rowList:[10,20,30],
+			viewrecords: true,
+			sortable:false,
+			emptyrecords:"没有数据！",
+			jsonReader : {  
+		
+		         root:"griddata",  
+		
+		         page: "currpage",  
+		
+		         total: "totalpages",  
+		
+		         records: "totalrecords"
+		
+		    }
 	});
 })
