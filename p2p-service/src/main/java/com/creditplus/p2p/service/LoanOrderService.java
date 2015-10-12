2daf32a7bf2b2e44f7c79da146f1b89ff97e30c8
Administrator <Administrator@PC201306252132>
2015-10-12 21:51:48 +0800

    dsfdf

--- p2p-service/src/main/java/com/creditplus/p2p/service/LoanOrderService.java
+++ p2p-service/src/main/java/com/creditplus/p2p/service/LoanOrderService.java
@@ -1,24 +1,22 @@
 package com.creditplus.p2p.service;
 
 import java.util.Map;
-
-import com.creditplus.p2p.common.annotation.ParamName;
-import com.creditplus.p2p.model.LoanOrderVO;
 import com.creditplus.p2p.model.PageVO;
 
 public interface LoanOrderService {
 
 	 //初审列表
-	 PageVO getCreditFirstTrialListWithPage(Map<?, ?> paramMap);
+	 PageVO getCreditFirstTrialListWithPage(@SuppressWarnings("rawtypes") Map paramMap);
 	 
 	 //复审列表
-	 PageVO getCreditReviewListWithPage(Map<?, ?> paramMap);
+	 PageVO getCreditReviewListWithPage(@SuppressWarnings("rawtypes") Map paramMap);
 	 
 	 //初审详情
-	 Map<?, ?> creditFirstTrial(@ParamName("loan_id")Integer loan_id);
+	 @SuppressWarnings("rawtypes")
+	 Map creditFirstTrial( Map paramMap) throws Exception;
 	 
-	 //复审详情
-	 Map<?, ?> creditReview(@ParamName("loan_id")Integer loan_id);
+	//复审详情
+	 @SuppressWarnings("rawtypes")
+	 Map creditReview(Map paramMap) throws Exception;
 	 
-//	 void setHandlerBy(@ParamName(""));
 }
